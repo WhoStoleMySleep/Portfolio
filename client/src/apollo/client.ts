ae035932238ef7f496bb874c8359fdcb7c25d9a6
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2023-08-02 15:55:58 +0700

    fix: edit server link

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://sreverokdlyasaita.onrender.com/graphql',
+  uri: 'https://sreverokdlyasaitaddsd.onrender.com/graphql',
   cache: new InMemoryCache(),
 }); 
 

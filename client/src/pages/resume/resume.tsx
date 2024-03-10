d52612ec903c83dde80df4d37b5954267026118a
whostolemysleep <thekostia5@gmail.com>
2024-03-10 14:19:00 +0700

    feat: new skills added

--- client/src/pages/resume/resume.tsx
+++ client/src/pages/resume/resume.tsx
@@ -1,55 +1,64 @@
-import clip from '../../assets/image/clip.svg';
-import ExperienceBlockquote from '../../components/ExperienceBlockquote/ExperienceBlockquote';
-import styles from './resume.module.scss'
-import { Link } from 'react-router-dom';
-import { EducationData, ExperienceData } from './resumedata';
-import EducationBlockquote from '../../components/EducationBlockquote/EducationBlockquote';
-import { useTranslation } from 'react-i18next';
+import clip from "../../assets/image/clip.svg";
+import ExperienceBlockquote from "../../components/ExperienceBlockquote/ExperienceBlockquote";
+import styles from "./resume.module.scss";
+import { Link } from "react-router-dom";
+import { EducationData, ExperienceData } from "./resumedata";
+import EducationBlockquote from "../../components/EducationBlockquote/EducationBlockquote";
+import { useTranslation } from "react-i18next";
 
-function Resume() {  
-  const { t } = useTranslation()
-  
+function Resume() {
+  const { t } = useTranslation();
+
   return (
     <main className={styles.resume}>
-      <h1 className={styles["resume__title"]}>{t("WhoStoleMySleep's Resume")}</h1>
-      <Link 
-        to={'/resume'}
-        // download={''} 
+      <h1 className={styles["resume__title"]}>
+        {t("WhoStoleMySleep's Resume")}
+      </h1>
+      <Link
+        to={"/resume"}
+        // download={''}
         // target="_blank"
-        className={styles['resume__file-link']}
+        className={styles["resume__file-link"]}
       >
-        <img src={clip} alt={''} className={styles["resume__icon"]} />
+        <img src={clip} alt={""} className={styles["resume__icon"]} />
         {t("File")}
       </Link>
       <article className={styles["resume__contacts"]}>
-        <Link className={styles["resume__number"]} to={'tel:+79133070592'}>
+        <Link className={styles["resume__number"]} to={"tel:+79133070592"}>
           +7 (913) 307-05-92
         </Link>
-        <Link className={styles["resume__email"]} to={'mailto:thekostia5@gmail.com'}>
+        <Link
+          className={styles["resume__email"]}
+          to={"mailto:thekostia5@gmail.com"}
+        >
           thekostia5@gmail.com
         </Link>
-        <Link className={styles["resume__site"]} to={'https://whostolemysleep.ru/'} target="_blank">
+        <Link
+          className={styles["resume__site"]}
+          to={"https://whostolemysleep.ru/"}
+          target="_blank"
+        >
           whostolemysleep.ru
         </Link>
       </article>
       <article className={styles["resume__about-me"]}>
-        <h4 className={styles["resume__article-title"]}>
-          {t("About Me")}
-        </h4>
+        <h4 className={styles["resume__article-title"]}>{t("About Me")}</h4>
         <p className={styles["resume__about-me-text"]}>
-          {t("I am a fullstack developer with over 4 years of experience in this field. My professional experience includes building custom websites as well as managing a team of developers. I am used to working with different technologies and programming languages such as HTML (JSX/TSX), CSS (SCSS), JavaScript (Typescript), React, Angular, Node.js, Figma and others. My goal is to create quality and user-friendly websites for clients to meet their needs and requirements. I am always up for new challenges and strive to continuously improve my skills and knowledge in web development.")}
+          {t(
+            "I am a fullstack developer with over 4 years of experience in this field. My professional experience includes building custom websites as well as managing a team of developers. I am used to working with different technologies and programming languages such as HTML (JSX/TSX), CSS (SCSS), JavaScript (Typescript), React, Angular, Node.js, Figma and others. My goal is to create quality and user-friendly websites for clients to meet their needs and requirements. I am always up for new challenges and strive to continuously improve my skills and knowledge in web development.",
+          )}
         </p>
         <p className={styles["resume__about-me-text"]}>
-          {t("I am passionate about programming and am constantly looking for ways to keep my skills up to date and learn new technologies. Currently, my goal is to develop my knowledge of Fullstak development and I am actively researching the latest trends in the field. I am also a big supporter of open source technologies and am constantly looking for ways to contribute to the community.")}
+          {t(
+            "I am passionate about programming and am constantly looking for ways to keep my skills up to date and learn new technologies. Currently, my goal is to develop my knowledge of Fullstak development and I am actively researching the latest trends in the field. I am also a big supporter of open source technologies and am constantly looking for ways to contribute to the community.",
+          )}
         </p>
       </article>
       <article className={styles["resume__experience"]}>
-        <h4 className={styles["resume__article-title"]}>
-          {t("Experience")}
-        </h4>
+        <h4 className={styles["resume__article-title"]}>{t("Experience")}</h4>
         <ul className="resume__blockquote-list">
           {ExperienceData.map((item, index) => (
-            <li key={index} className='resume__blockquote-list-element'>
+            <li key={index} className="resume__blockquote-list-element">
               <ExperienceBlockquote ExperienceData={item} />
             </li>
           ))}
@@ -59,7 +68,7 @@ function Resume() {
         <h4 className={styles["resume__article-title"]}>{t("Education")}</h4>
         <ul className="resume__blockquote-list">
           {EducationData.map((item, index) => (
-            <li key={index} className='resume__blockquote-list-element'>
+            <li key={index} className="resume__blockquote-list-element">
               <EducationBlockquote EducationData={item} />
             </li>
           ))}
@@ -77,7 +86,7 @@ function Resume() {
           <li className={styles["resume__frameworks"]}>
             <p className={styles["resume__skill-title"]}>{t("Frameworks: ")}</p>
             <span className={styles["resume__all-learned"]}>
-              React, Next.js, Angular, Node.js, Express, Laravel
+              React, Next.js, Angular, Node.js, Express, Laravel, Vue, Nuxt
             </span>
           </li>
           <li className={styles["resume__cms"]}>
@@ -89,13 +98,14 @@ function Resume() {
           <li className={styles["resume__tools"]}>
             <p className={styles["resume__skill-title"]}>{t("Tools: ")}</p>
             <span className={styles["resume__all-learned"]}>
-              Git, MySQL, REST API, Graphql, Jira, Webpack, Jest, Redux, Mobx, Figma
+              Git, MySQL, REST API, Graphql, Webpack, Jest, Redux, Mobx, Pinia,
+              Figma
             </span>
           </li>
         </ul>
       </article>
     </main>
-  )
+  );
 }
 
 export default Resume;

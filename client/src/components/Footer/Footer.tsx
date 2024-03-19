354bd19f577603ef0411698cec1ad03900da8a25
whostolemysleep <thekostia5@gmail.com>
2024-03-19 19:37:51 +0700

    feat: update copyright

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,10 +1,10 @@
-import styles from './Footer.module.scss';
+import styles from "./Footer.module.scss";
 
 function BlogList() {
   return (
     <footer className={styles.footer}>
       <p className={styles.footer__copyright}>
-        Copyright {(new Date()).getFullYear()} WhoStoleMySleep
+        Copyright © {new Date().getFullYear()} WhoStoleMySleep
       </p>
     </footer>
   );
